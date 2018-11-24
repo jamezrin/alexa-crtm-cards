914480e760089a6c7fc58e02d19f476ef08b9fca
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2018-11-24 23:24:03 +0100

    Update

--- src/main/java/com/jamezrin/alexaskills/crtmcards/skill/handlers/ExpirationDateIntentHandler.java
+++ src/main/java/com/jamezrin/alexaskills/crtmcards/skill/handlers/ExpirationDateIntentHandler.java
@@ -17,18 +17,12 @@ import com.amazon.ask.attributes.AttributesManager;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.*;
-import com.amazon.ask.model.canfulfill.CanFulfillIntent;
-import com.amazon.ask.model.canfulfill.CanFulfillSlot;
-import com.amazon.ask.model.dialog.DelegateDirective;
 import com.amazon.ask.response.ResponseBuilder;
-import com.jamezrin.alexaskills.crtmcards.AppConsts;
-import com.jamezrin.alexaskills.crtmcards.skill.CardInfo;
 
 import java.util.Map;
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
-import static com.jamezrin.alexaskills.crtmcards.AppConsts.SKILL_CARD_TITLE;
 
 public class ExpirationDateIntentHandler implements RequestHandler {
     @Override
@@ -45,55 +39,38 @@ public class ExpirationDateIntentHandler implements RequestHandler {
         IntentRequest intentRequest = (IntentRequest) request;
         Intent intent = intentRequest.getIntent();
 
-        String prefix = (String) attributes.get("ttp_prefix");
-        String number = (String) attributes.get("ttp_number");
+        Map<String, Slot> slots = intent.getSlots();
 
-        StringBuilder speech = new StringBuilder();
-        if (prefix == null || number == null) {
-            //speech.append("Antes de poder ayudarte necesito que me des unos numeros de tu tarjeta. ");
-            //speech.append("Los puedes encontrar al lado de tu foto en tu tarjeta de transportes.");
+        Slot prefixSlot = slots.get("prefix");
+        if (prefixSlot.getValue() != null) {
+            attributes.put("ttp_prefix", prefixSlot.getValue());
         }
 
+        Slot numberSlot = slots.get("number");
+        if (numberSlot.getValue() != null) {
+            attributes.put("ttp_number", numberSlot.getValue());
+        }
+
+        if (prefixSlot.getValue() != null || numberSlot.getValue() != null) {
+            attributesManager.setPersistentAttributes(attributes);
+            attributesManager.savePersistentAttributes();
+        }
+
+        String prefix = (String) attributes.get("ttp_prefix");
         if (prefix == null) {
-            speech.append("Dime los tres ultimos digitos de la primera linea");
-            builder.withSpeech(speech.toString());
+            builder.withSpeech("Dame tu prefijo");
             builder.addElicitSlotDirective("prefix", intent);
             return builder.build();
         }
 
+        String number = (String) attributes.get("ttp_number");
         if (number == null) {
-            speech.append("Dime todos los digitos de la segunda linea");
-            builder.withSpeech(speech.toString());
+            builder.withSpeech("Dame tu numero");
             builder.addElicitSlotDirective("number", intent);
             return builder.build();
         }
 
-        Map<String, Slot> slots = intent.getSlots();
-
-        Slot prefixSlot = slots.get("prefix");
-        Slot numberSlot = slots.get("number");
-
-        if (prefixSlot.getValue() != null || numberSlot.getValue() != null) {
-            if (prefixSlot.getValue() != null) {
-                speech.append("Tienes un prefijo asignado: ");
-                speech.append(prefixSlot.getValue());
-                attributes.put("ttp_prefix", prefixSlot.getValue());
-            }
-
-            if (numberSlot.getValue() != null) {
-                speech.append("Tienes un numero asignado: ");
-                speech.append(numberSlot.getValue());
-                attributes.put("ttp_number", numberSlot.getValue());
-            }
-
-            input.getAttributesManager().setPersistentAttributes(attributes);
-            input.getAttributesManager().savePersistentAttributes();
-        }
-
-
-
-
-        builder.withSpeech(speech.toString());
+        builder.withSpeech("Tu prefijo es " + prefix + " y tu numero es " + number);
         return builder.build();
     }
 }
